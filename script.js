068db269c4541201caadb206ddfdc962a221f843
242086 <242086@edu.fa.ru>
2026-02-26 15:10:38 +0300

    Added basic UI and functionality for adding tasks

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function addTask() {
+    const input = document.getElementById('newTaskInput');
+    const taskText = input.value.trim();
 
+    if(taskText) {
+        const taskList = document.getElementById('taskList');
+        const newItem = document.createElement('li');
+        newItem.textContent = taskText;
+        taskList.appendChild(newItem);
+        input.value = '';
+    }
+}
